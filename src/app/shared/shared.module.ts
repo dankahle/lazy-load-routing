f9ff5bd113342b713f9cec43b9cb7f45dd168c86
dan kahle <dan.kahle@gmail.com>
2018-04-26 09:05:05 -0700

    working with core import/export in shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import {RouterModule} from "@angular/router";
 import { MainComponent } from './main/main.component';
+import {CoreModule} from "../core/core.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CoreModule
+  ],
+  exports: [
+    CoreModule
   ],
   declarations: [HomeComponent, NotFoundComponent, MainComponent]
   // exports: [HomeComponent, NotFoundComponent, MainComponent]
