58ea74488825951e1e8279e93f85761e7f3a56c7
dan kahle <dan.kahle@gmail.com>
2018-04-26 08:10:59 -0700

    added module and component create logs

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('cust list const');
+  }
 
   ngOnInit() {
   }
--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('orders list const');
+
+  }
 
   ngOnInit() {
   }
